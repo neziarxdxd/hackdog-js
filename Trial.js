6b48a71612062c451838dff8cb2bb28d923819f2
JemrickD-01 <56538745+JemrickD-01@users.noreply.github.com>
2020-11-10 10:16:21 +0800

    Add files via upload

---
+++ Trial.js
@@ -0,0 +1,37 @@
+To_do = [
+{
+	date_todo:"03-15-00", name:"Matutulog", isDone:true
+},
+{
+	date_todo:"02-08-10", name:"Natutulog", isDone:true
+},
+{
+	date_todo:"12-29-15", name:"Gigising", isDone:false
+},
+{
+	date_todo:"05-18-04", name:"Aalis", isDone:false
+},
+{
+	date_todo:"11-11-11", name:"Kakain", isDone:false
+},
+{
+	date_todo:"12-12-12", name:"Matutulog", isDone:true
+},
+]
+To_do.push({
+	date_todo:"1-11-11", name:"Kain", isDone:false
+});
+
+To_do.map((job)=>(
+	console.log(job)
+	));
+
+delete To_do[0]
+console.log("************")
+To_do[2] = {date_todo:"07-12-03", name:"Okayyy", isDone:true}
+
+To_do[1]["name"]="Jemrick";//Specific Update
+
+To_do.map((job)=>(
+	console.log(job)
+	));
---
+++ tutorial.js
@@ -0,0 +1,19 @@
+//console.log("Connected");
+
+books = [
+{
+	id:1980, name:"Jemrick", date_published:2000
+},
+{
+	id:1981, name:"Jemrick1", date_published:2001
+},
+{
+	id:1982, name:"Jemrick2", date_published:2002
+},
+{
+	id:1983, name:"Jemrick3", date_published:2003
+},
+]
+books.map((book)=>(
+	console.log(book[name])
+	))
